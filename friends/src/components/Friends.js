6a03fbdc4f7cf366a3d569a176496d928f32ff5a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-24 14:49:00 -0600

    Get friends from API

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from "react";
 
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 function Friends() {
   const [friends, setFriends] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => console.log("Failed to get friends from API", err));
+  }, []);
 
-  return <div>Friends</div>;
+  return (
+    <div>
+      <h2>Friends</h2>
+      <div>
+        {friends.map(friend => (
+          <div key={friend.id}>
+            <code>{JSON.stringify(friend)}</code>
+            <br />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Friends;
--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -1,1 +1,10 @@
+import axios from "axios";
+import { TOKEN_KEY } from "../constants";
 
+export function axiosWithAuth() {
+  return axios.create({
+    headers: {
+      Authorization: localStorage.getItem(TOKEN_KEY)
+    }
+  });
+}
