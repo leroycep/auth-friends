0cd4a07d50e16d74a4742ff66ebeb291da015b1b
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-24 15:39:24 -0600

    Link to friends' pages

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FRIENDS_ROUTE } from "../constants";
 
 function Friends(props) {
   return (
     <div>
       <h2>Friends</h2>
-      <div>
+      <ul>
         {props.friends.map(friend => (
-          <div key={friend.id}>
-            <code>{JSON.stringify(friend)}</code>
-            <br />
-          </div>
+          <li key={friend.id}>
+            <Link to={`${FRIENDS_ROUTE}/${friend.id}`}>{friend.name}</Link>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
